9755323bdaf882ba0034f0a0feab2884f1962dbf
JEN023__ <lunar_halo_@naver.com>
2026-02-14 15:46:30 +0900

    fix: image on local(Postlist - thumbnail)

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -10,9 +10,10 @@ const POST_ITEM_DATA = {
    summary:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
   thumbnail:
-    '<https://ji5485.github.io/static/e4f34c558ae8e8235ff53b0311085796/4d854/javascript-core-concept-summary-function-1.webp>',
+    'https://raw.githubusercontent.com/Ganadalam/Gatsby/main/src/components/images/thumbnail.png',
   link: '<https://www.google.co.kr/>',
 }
+// 추후 개선 후보: StaticImage (지금은 임시 데이터임)
 
 const PostListWrapper = styled.div`
   display: grid;
