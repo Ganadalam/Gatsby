ff2c8818ffe0bf736293fc8d1343cb2d3b4b0d19
JEN023__ <lunar_halo_@naver.com>
2026-02-14 14:09:34 +0900

    fix: footer

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -1,18 +1,27 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
+import profileImg from '../images/profile.png'  // 경로 맞게 수정
 
-
-const PROFILE_IMAGE_LINK =
-  'https://raw.githubusercontent.com/Ganadalam/Gatsby/main/src/images/profile.png'
 const ProfileImageWrapper = styled.img`
-  width: 120px;
-  height: 120px;
+  width: 150px;
+  height: 150px;
   margin-bottom: 30px;
   border-radius: 50%;
+  object-fit: cover;
+  object-position: center;
 `
 
+// const PROFILE_IMAGE_LINK =
+//   'https://raw.githubusercontent.com/Ganadalam/Gatsby/main/src/images/profile.png'
+// const ProfileImageWrapper = styled.img`
+//   width: 120px;
+//   height: 120px;
+//   margin-bottom: 30px;
+//   border-radius: 50%;
+// `
+
 const ProfileImage: FunctionComponent = function () {
-  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+  return <ProfileImageWrapper src={profileImg} alt="Profile Image" />
 }
 
 export default ProfileImage
